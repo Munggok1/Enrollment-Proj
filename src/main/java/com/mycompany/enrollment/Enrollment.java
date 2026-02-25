6e5598f3ddeb9ee0e182e9cdd0b07200b5ce8e9e
PHandrei01 <PHandrei01@LAPTOP-M1BJLB8E>
2026-02-25 22:54:32 +0800

    Admin Design

---
+++ src/main/java/com/mycompany/enrollment/Enrollment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.enrollment;
+
+/**
+ *
+ * @author PHandrei01
+ */
+public class Enrollment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
